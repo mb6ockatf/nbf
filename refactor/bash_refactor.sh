64d3227a26faa4bad66554530d152d8c04c6af18
mb6ockatf <mdddmmmm@yandex.ru>
2023-05-22 00:13:38 +0300

    feat(bash_refactor.sh): shfmt refactoring

--- refactor/bash_refactor.sh
+++ refactor/bash_refactor.sh
@@ -3,12 +3,14 @@
 # refactor *.sh files recirsively in the current folder
 # original files are renamed into oldname.orig, like `artictic style` does with
 # c files
-# basically, just removes empty lines and adds ones after code blocks
+# basically, just removes empty lines, adds ones after code blocks and does
+# `shfmt` formatting
 # output: formatted sh files
 ###############################################################################
 refactor::sh(){
 	while IFS= read -r -d '' filename; do
 		cp "$filename" "${filename}.orig"
 		sed -i '/^\s*$/d; /.*\}$/G' "$filename"
+		shfmt -l -w -ln bash -ci -fn -s "$filename"
 	done <	<(find ./ -type f -iname "*.sh")
 }
